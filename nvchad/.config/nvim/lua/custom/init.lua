248810ad69a70a1f323fe73070bb2ba6c3671ba6
Patrick Kerwood <patrick@kerwood.dk>
2024-02-25 10:13:10 +0100

    update

---
+++ nvchad/.config/nvim/lua/custom/init.lua
@@ -0,0 +1,21 @@
+local autocmd = vim.api.nvim_create_autocmd
+local opt = vim.opt
+
+-- Remove trailing whitespace in save
+autocmd("BufWritePre", {
+  pattern = "*",
+  command = "%s/\\s\\+$//e",
+})
+
+-- Auto resize panes when resizing nvim window
+autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
+
+
+opt.ignorecase = true     -- ignore case in search patterns
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.whichwrap = 'b,s'     -- Default value
+opt.iskeyword:append("-")
