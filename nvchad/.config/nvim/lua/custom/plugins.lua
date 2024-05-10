672b07a833d02c888ca3e25259597f363ece087a
Patrick Kerwood <patrick@kerwood.dk>
2024-05-10 10:17:17 +0200

    added nvim-surrond

--- nvchad/.config/nvim/lua/custom/plugins.lua
+++ nvchad/.config/nvim/lua/custom/plugins.lua
@@ -53,6 +53,18 @@ local plugins = {
     },
   },
 
+  -- https://github.com/kylechui/nvim-surround
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+
   {
     "hrsh7th/nvim-cmp",
     opts = function()
