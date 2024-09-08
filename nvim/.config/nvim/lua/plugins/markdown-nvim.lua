53b8a21b2d713fa282dc7a691e131acfd29d5183
Patrick Kerwood <patrick@kerwood.dk>
2024-09-08 21:29:53 +0200

    added markdown plugins

---
+++ nvim/.config/nvim/lua/plugins/markdown-nvim.lua
@@ -0,0 +1,10 @@
+-- Configurable tools for working with markdown files in Neovim.
+-- https://github.com/tadmccorkle/markdown.nvim
+
+return {
+  {
+    "tadmccorkle/markdown.nvim",
+    ft = "markdown",
+    config = true,
+  }
+}
