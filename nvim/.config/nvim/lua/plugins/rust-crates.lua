2600fd635394ec16438a6c3c19e0c7b95cb69784
Patrick Kerwood <patrick@kerwood.dk>
2025-02-26 08:32:33 +0100

    updated rust-crates to 0.6.0

--- nvim/.config/nvim/lua/plugins/rust-crates.lua
+++ nvim/.config/nvim/lua/plugins/rust-crates.lua
@@ -2,28 +2,28 @@
 -- https://github.com/Saecki/crates.nvim
 
 local options = {
-    popup = {
-      autofocus = true,
-      border = "rounded",
-    },
+  popup = {
+    autofocus = true,
+    border = "rounded",
+  },
 }
 
-local config = function ()
+local config = function()
   local crates = require("crates")
   crates.setup(options)
 
-  vim.keymap.set("n", "<leader>cf", function() crates.show_features_popup() end, { desc = "Crates - Show Features"})
-  vim.keymap.set("n", "<leader>cv", function() crates.show_versions_popup() end, { desc = "Crates - Show Versions"})
-  vim.keymap.set("n", "<leader>cua", function() crates.upgrade_all_crates() end, { desc = "Crates - Upgrade All Crates"})
-  vim.keymap.set("n", "<leader>cu", function() crates.upgrade_crate() end, { desc = "Crates - Upgrade Crate"})
+  vim.keymap.set("n", "<leader>cf", function() crates.show_features_popup() end, { desc = "Crates - Show Features" })
+  vim.keymap.set("n", "<leader>cv", function() crates.show_versions_popup() end, { desc = "Crates - Show Versions" })
+  vim.keymap.set("n", "<leader>cua", function() crates.upgrade_all_crates() end, { desc = "Crates - Upgrade All Crates" })
+  vim.keymap.set("n", "<leader>cu", function() crates.upgrade_crate() end, { desc = "Crates - Upgrade Crate" })
   vim.keymap.set("n", "<leader>cs", function() crates.show_crate_popup() end, { desc = "Crates - Show Crate" })
-  vim.keymap.set("v", "<leader>cu", function() crates.upgrade_crates() end, { desc = "Crates - Upgrade Selected Crates"})
+  vim.keymap.set("v", "<leader>cu", function() crates.upgrade_crates() end, { desc = "Crates - Upgrade Selected Crates" })
 end
 
 return {
   {
     "saecki/crates.nvim",
-    tag = "v0.5.2",
+    tag = "v0.6.0",
     event = "BufReadPre Cargo.toml",
     config = config,
     dependencies = {
